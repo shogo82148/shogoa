1df5f7572e6886a6b3bd87360078f43d5f5d29fc
ICHINOSE Shogo <shogo82148@gmail.com>
2024-07-28 21:36:59 +0900

    refactor design/api_test.go

--- design/api_test.go
+++ design/api_test.go
@@ -1,122 +1,128 @@
-package design_test
+package design
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/shogo82148/shogoa/design"
+	"slices"
+	"testing"
+
 	"github.com/shogo82148/shogoa/dslengine"
 )
 
-var _ = Describe("CanonicalIdentifier", func() {
-	var id string
-	var canonical string
+func TestCanonicalIdentifier(t *testing.T) {
+	tests := []struct {
+		id   string
+		want string
+	}{
+		{
+			id:   "application/json",
+			want: "application/json",
+		},
+		{
+			id:   "application/json+xml; foo=bar",
+			want: "application/json; foo=bar",
+		},
+	}
 
-	JustBeforeEach(func() {
-		canonical = design.CanonicalIdentifier(id)
-	})
-
-	Context("with a canonical identifier", func() {
-		BeforeEach(func() {
-			id = "application/json"
+	for _, tt := range tests {
+		t.Run(tt.id, func(t *testing.T) {
+			got := CanonicalIdentifier(tt.id)
+			if got != tt.want {
+				t.Errorf("expected %v, got %v", tt.want, got)
+			}
 		})
+	}
+}
 
-		It("returns it", func() {
-			Ω(canonical).Should(Equal(id))
+func TestExtractWildcards(t *testing.T) {
+	tests := []struct {
+		path string
+		want []string
+	}{
+		{
+			path: "/foo",
+			want: nil,
+		},
+		{
+			path: "/a/:foo/:bar/b/:baz/c",
+			want: []string{"foo", "bar", "baz"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.path, func(t *testing.T) {
+			got := ExtractWildcards(tt.path)
+			if !slices.Equal(got, tt.want) {
+				t.Errorf("expected %v, got %v", tt.want, got)
+			}
 		})
-	})
+	}
+}
 
-	Context("with a non canonical identifier", func() {
-		BeforeEach(func() {
-			id = "application/json+xml; foo=bar"
+func TestMediaTypeRoot_DSLName(t *testing.T) {
+	root := MediaTypeRoot{}
+	Design.MediaTypes = make(map[string]*MediaTypeDefinition)
+
+	if root.DSLName() == "" {
+		t.Error("DSLName is empty")
+	}
+}
+
+func TestMediaTypeRoot_DependsOn(t *testing.T) {
+	root := MediaTypeRoot{}
+	Design.MediaTypes = make(map[string]*MediaTypeDefinition)
+	if !slices.Equal(root.DependsOn(), []dslengine.Root{Design}) {
+		t.Error("DependsOn is invalid")
+	}
+}
+
+func TestMediaTypeRoot_IterateSets(t *testing.T) {
+	tests := []struct {
+		name string
+		root MediaTypeRoot
+		set  []string
+	}{
+		{
+			name: "empty",
+			root: MediaTypeRoot{},
+			set:  []string{},
+		},
+		{
+			name: "one",
+			root: MediaTypeRoot{
+				"foo": &MediaTypeDefinition{Identifier: "application/json"},
+			},
+			set: []string{"foo"},
+		},
+		{
+			name: "two",
+			root: MediaTypeRoot{
+				"foo": &MediaTypeDefinition{Identifier: "application/json"},
+				"bar": &MediaTypeDefinition{Identifier: "application/xml"},
+			},
+			set: []string{"foo", "bar"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			root := tt.root
+			Design.MediaTypes = make(map[string]*MediaTypeDefinition)
+			var sets []dslengine.DefinitionSet
+			it := func(s dslengine.DefinitionSet) error {
+				sets = append(sets, s)
+				return nil
+			}
+			root.IterateSets(it)
+			if len(sets) != 1 {
+				t.Errorf("expected 1, got %d", len(sets))
+			}
+			if len(sets[0]) != len(tt.set) {
+				t.Errorf("expected %d, got %d", len(root), len(sets[0]))
+			}
+			for i, set := range sets[0] {
+				if set != root[tt.set[i]] {
+					t.Errorf("expected %v, got %v", root[tt.set[i]], set)
+				}
+			}
 		})
-
-		It("cannibalizes it", func() {
-			Ω(canonical).Should(Equal("application/json; foo=bar"))
-		})
-	})
-})
-
-var _ = Describe("ExtractWildcards", func() {
-	var path string
-	var wcs []string
-
-	JustBeforeEach(func() {
-		wcs = design.ExtractWildcards(path)
-	})
-
-	Context("with a path with no wildcard", func() {
-		BeforeEach(func() {
-			path = "/foo"
-		})
-
-		It("returns the empty slice", func() {
-			Ω(wcs).Should(HaveLen(0))
-		})
-	})
-
-	Context("with a path with wildcards", func() {
-		BeforeEach(func() {
-			path = "/a/:foo/:bar/b/:baz/c"
-		})
-
-		It("extracts them", func() {
-			Ω(wcs).Should(Equal([]string{"foo", "bar", "baz"}))
-		})
-	})
-})
-
-var _ = Describe("MediaTypeRoot", func() {
-	var root design.MediaTypeRoot
-
-	BeforeEach(func() {
-		design.Design.MediaTypes = make(map[string]*design.MediaTypeDefinition)
-		root = design.MediaTypeRoot{}
-	})
-
-	It("has a non empty DSL name", func() {
-		Ω(root.DSLName()).ShouldNot(BeEmpty())
-	})
-
-	It("depends on the shogoa API design root", func() {
-		Ω(root.DependsOn()).Should(Equal([]dslengine.Root{design.Design}))
-	})
-
-	It("iterates over the generated media types when it's empty", func() {
-		var sets []dslengine.DefinitionSet
-		it := func(s dslengine.DefinitionSet) error {
-			sets = append(sets, s)
-			return nil
-		}
-		root.IterateSets(it)
-		Ω(sets).Should(HaveLen(1))
-		Ω(sets[0]).Should(BeEmpty())
-	})
-
-	It("iterates over the generated media types", func() {
-		var sets []dslengine.DefinitionSet
-		it := func(s dslengine.DefinitionSet) error {
-			sets = append(sets, s)
-			return nil
-		}
-		root["foo"] = &design.MediaTypeDefinition{Identifier: "application/json"}
-		root.IterateSets(it)
-		Ω(sets).Should(HaveLen(1))
-		Ω(sets[0]).Should(HaveLen(1))
-		Ω(sets[0][0]).Should(Equal(root["foo"]))
-	})
-
-	It("iterates over the generated media types in order", func() {
-		var sets []dslengine.DefinitionSet
-		it := func(s dslengine.DefinitionSet) error {
-			sets = append(sets, s)
-			return nil
-		}
-		root["foo"] = &design.MediaTypeDefinition{Identifier: "application/json"}
-		root["bar"] = &design.MediaTypeDefinition{Identifier: "application/xml"}
-		root.IterateSets(it)
-		Ω(sets).Should(HaveLen(1))
-		Ω(sets[0]).Should(HaveLen(2))
-		Ω(sets[0][0]).Should(Equal(root["foo"]))
-		Ω(sets[0][1]).Should(Equal(root["bar"]))
-	})
-})
+	}
+}
