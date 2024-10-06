9e6541f71de0b81d6dabbaffb3e609004c63a8ce
ICHINOSE Shogo <shogo82148@gmail.com>
2024-10-06 15:37:08 +0900

    refactor design/apidsl/security_test.go

--- design/apidsl/security_test.go
+++ design/apidsl/security_test.go
@@ -1,26 +1,27 @@
 package apidsl_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"testing"
+
 	"github.com/shogo82148/shogoa/design"
 	"github.com/shogo82148/shogoa/design/apidsl"
 	"github.com/shogo82148/shogoa/dslengine"
 )
 
-var _ = Describe("Security", func() {
-	BeforeEach(func() {
+func TestSecurity(t *testing.T) {
+	t.Run("should have no security DSL when none are defined", func(t *testing.T) {
 		dslengine.Reset()
-	})
-
-	It("should have no security DSL when none are defined", func() {
 		apidsl.API("secure", nil)
-		dslengine.Run()
-		Ω(design.Design.SecuritySchemes).Should(BeNil())
-		Ω(dslengine.Errors).ShouldNot(HaveOccurred())
+		if err := dslengine.Run(); err != nil {
+			t.Fatal(err)
+		}
+		if design.Design.SecuritySchemes != nil {
+			t.Errorf("SecuritySchemes = %v; want nil", design.Design.SecuritySchemes)
+		}
 	})
 
-	It("should be the fully valid and well defined, live on the happy path", func() {
+	t.Run("should be the fully valid and well defined, live on the happy path", func(t *testing.T) {
+		dslengine.Reset()
 		apidsl.API("secure", func() {
 			apidsl.Host("example.com")
 			apidsl.Scheme("http")
@@ -48,193 +49,243 @@ var _ = Describe("Security", func() {
 				apidsl.Scope("user:write", "Write users")
 			})
 		})
+		if err := dslengine.Run(); err != nil {
+			t.Fatal(err)
+		}
+		if len(design.Design.SecuritySchemes) != 4 {
+			t.Errorf("SecuritySchemes = %v; want 4", design.Design.SecuritySchemes)
+		}
 
-		dslengine.Run()
+		if design.Design.SecuritySchemes[0].Kind != design.BasicAuthSecurityKind {
+			t.Errorf("SecuritySchemes[0].Kind = %v; want %v", design.Design.SecuritySchemes[0].Kind, design.BasicAuthSecurityKind)
+		}
+		if design.Design.SecuritySchemes[0].Description != "desc" {
+			t.Errorf("SecuritySchemes[0].Description = %v; want desc", design.Design.SecuritySchemes[0].Description)
+		}
 
-		Ω(dslengine.Errors).ShouldNot(HaveOccurred())
-		Ω(design.Design.SecuritySchemes).Should(HaveLen(4))
+		if design.Design.SecuritySchemes[1].Kind != design.OAuth2SecurityKind {
+			t.Errorf("SecuritySchemes[1].Kind = %v; want %v", design.Design.SecuritySchemes[1].Kind, design.OAuth2SecurityKind)
+		}
+		if design.Design.SecuritySchemes[1].AuthorizationURL != "http://example.com/auth" {
+			t.Errorf("SecuritySchemes[1].AuthorizationURL = %v; want %v", design.Design.SecuritySchemes[1].AuthorizationURL, "http://example.com/auth")
+		}
+		if design.Design.SecuritySchemes[1].TokenURL != "http://example.com/token" {
+			t.Errorf("SecuritySchemes[1].TokenURL = %v; want %v", design.Design.SecuritySchemes[1].TokenURL, "http://example.com/token")
+		}
+		if design.Design.SecuritySchemes[1].Flow != "accessCode" {
+			t.Errorf("SecuritySchemes[1].Flow = %v; want %v", design.Design.SecuritySchemes[1].Flow, "accessCode")
+		}
 
-		Ω(design.Design.SecuritySchemes[0].Kind).Should(Equal(design.BasicAuthSecurityKind))
-		Ω(design.Design.SecuritySchemes[0].Description).Should(Equal("desc"))
+		if design.Design.SecuritySchemes[2].Kind != design.APIKeySecurityKind {
+			t.Errorf("SecuritySchemes[2].Kind = %v; want %v", design.Design.SecuritySchemes[2].Kind, design.APIKeySecurityKind)
+		}
+		if design.Design.SecuritySchemes[2].In != "query" {
+			t.Errorf("SecuritySchemes[2].In = %v; want %v", design.Design.SecuritySchemes[2].In, "query")
+		}
+		if design.Design.SecuritySchemes[2].Name != "access_token" {
+			t.Errorf("SecuritySchemes[2].Name = %v; want %v", design.Design.SecuritySchemes[2].Name, "access_token")
+		}
 
-		Ω(design.Design.SecuritySchemes[1].Kind).Should(Equal(design.OAuth2SecurityKind))
-		Ω(design.Design.SecuritySchemes[1].AuthorizationURL).Should(Equal("http://example.com/auth"))
-		Ω(design.Design.SecuritySchemes[1].TokenURL).Should(Equal("http://example.com/token"))
-		Ω(design.Design.SecuritySchemes[1].Flow).Should(Equal("accessCode"))
-
-		Ω(design.Design.SecuritySchemes[2].Kind).Should(Equal(design.APIKeySecurityKind))
-		Ω(design.Design.SecuritySchemes[2].In).Should(Equal("query"))
-		Ω(design.Design.SecuritySchemes[2].Name).Should(Equal("access_token"))
-
-		Ω(design.Design.SecuritySchemes[3].Kind).Should(Equal(design.JWTSecurityKind))
-		Ω(design.Design.SecuritySchemes[3].TokenURL).Should(Equal("http://example.com/token"))
-		Ω(design.Design.SecuritySchemes[3].Scopes).Should(HaveLen(2))
+		if design.Design.SecuritySchemes[3].Kind != design.JWTSecurityKind {
+			t.Errorf("SecuritySchemes[3].Kind = %v; want %v", design.Design.SecuritySchemes[3].Kind, design.JWTSecurityKind)
+		}
+		if design.Design.SecuritySchemes[3].TokenURL != "http://example.com/token" {
+			t.Errorf("SecuritySchemes[3].TokenURL = %v; want %v", design.Design.SecuritySchemes[3].TokenURL, "http://example.com/token")
+		}
+		if len(design.Design.SecuritySchemes[3].Scopes) != 2 {
+			t.Errorf("SecuritySchemes[3].Scopes = %v; want 2", design.Design.SecuritySchemes[3].Scopes)
+		}
 	})
 
-	Context("with basic security", func() {
-		It("should fail because of duplicate In declaration", func() {
-			apidsl.API("", func() {
-				apidsl.BasicAuthSecurity("broken_basic_authz", func() {
-					apidsl.Description("desc")
-					apidsl.Header("Authorization")
-					apidsl.Query("access_token")
-				})
+	t.Run("should fallback properly to lower-level security", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.JWTSecurity("jwt", func() {
+				apidsl.TokenURL("/token")
+				apidsl.Scope("read", "Read")
+				apidsl.Scope("write", "Write")
 			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
+			apidsl.BasicAuthSecurity("password")
+
+			apidsl.Security("jwt")
 		})
-
-		It("should fail because of invalid declaration of OAuth2Flow", func() {
-			apidsl.API("", func() {
-				apidsl.BasicAuthSecurity("broken_basic_authz", func() {
-					apidsl.Description("desc")
-					apidsl.ImplicitFlow("invalid")
-				})
+		apidsl.Resource("one", func() {
+			apidsl.Action("first", func() {
+				apidsl.Routing(apidsl.GET("/first"))
+				apidsl.NoSecurity()
 			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
+			apidsl.Action("second", func() {
+				apidsl.Routing(apidsl.GET("/second"))
+			})
 		})
+		apidsl.Resource("two", func() {
+			apidsl.Security("password")
 
-		It("should fail because of invalid declaration of TokenURL", func() {
-			apidsl.API("", func() {
-				apidsl.BasicAuthSecurity("broken_basic_authz", func() {
-					apidsl.Description("desc")
-					apidsl.TokenURL("/token")
-				})
+			apidsl.Action("third", func() {
+				apidsl.Routing(apidsl.GET("/third"))
 			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
-		})
-
-		It("should fail because of invalid declaration of TokenURL", func() {
-			apidsl.API("", func() {
-				apidsl.BasicAuthSecurity("broken_basic_authz", func() {
-					apidsl.Description("desc")
-					apidsl.TokenURL("in valid")
-				})
-			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
-		})
-
-		It("should fail because of invalid declaration of Header", func() {
-			apidsl.API("", func() {
-				apidsl.BasicAuthSecurity("broken_basic_authz", func() {
-					apidsl.Description("desc")
-					apidsl.Header("invalid")
-				})
-			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
-		})
-	})
-
-	Context("with oauth2 security", func() {
-		It("should pass with valid values when well defined", func() {
-			apidsl.API("", func() {
-				apidsl.Host("example.com")
-				apidsl.Scheme("http")
-				apidsl.OAuth2Security("googAuthz", func() {
-					apidsl.Description("Use Goog's Auth")
-					apidsl.AccessCodeFlow("/auth", "/token")
-					apidsl.Scope("scope:1", "Desc 1")
-					apidsl.Scope("scope:2", "Desc 2")
-				})
-			})
-			apidsl.Resource("one", func() {
-				apidsl.Action("first", func() {
-					apidsl.Routing(apidsl.GET("/first"))
-					apidsl.Security("googAuthz", func() {
-						apidsl.Scope("scope:1")
-					})
-				})
-			})
-
-			dslengine.Run()
-
-			Ω(dslengine.Errors).ShouldNot(HaveOccurred())
-			Ω(design.Design.SecuritySchemes).Should(HaveLen(1))
-			scheme := design.Design.SecuritySchemes[0]
-			Ω(scheme.Description).Should(Equal("Use Goog's Auth"))
-			Ω(scheme.AuthorizationURL).Should(Equal("http://example.com/auth"))
-			Ω(scheme.TokenURL).Should(Equal("http://example.com/token"))
-			Ω(scheme.Flow).Should(Equal("accessCode"))
-			Ω(scheme.Scopes["scope:1"]).Should(Equal("Desc 1"))
-			Ω(scheme.Scopes["scope:2"]).Should(Equal("Desc 2"))
-		})
-
-		It("should fail because of invalid declaration of Header", func() {
-			apidsl.API("", func() {
-				apidsl.OAuth2Security("googAuthz", func() {
-					apidsl.Header("invalid")
-				})
-			})
-			dslengine.Run()
-			Ω(dslengine.Errors).Should(HaveOccurred())
-		})
-
-	})
-
-	Context("with resources and actions", func() {
-		It("should fallback properly to lower-level security", func() {
-			apidsl.API("", func() {
-				apidsl.JWTSecurity("jwt", func() {
-					apidsl.TokenURL("/token")
-					apidsl.Scope("read", "Read")
-					apidsl.Scope("write", "Write")
-				})
-				apidsl.BasicAuthSecurity("password")
-
+			apidsl.Action("fourth", func() {
+				apidsl.Routing(apidsl.GET("/fourth"))
 				apidsl.Security("jwt")
 			})
-			apidsl.Resource("one", func() {
-				apidsl.Action("first", func() {
-					apidsl.Routing(apidsl.GET("/first"))
-					apidsl.NoSecurity()
-				})
-				apidsl.Action("second", func() {
-					apidsl.Routing(apidsl.GET("/second"))
-				})
-			})
-			apidsl.Resource("two", func() {
-				apidsl.Security("password")
-
-				apidsl.Action("third", func() {
-					apidsl.Routing(apidsl.GET("/third"))
-				})
-				apidsl.Action("fourth", func() {
-					apidsl.Routing(apidsl.GET("/fourth"))
-					apidsl.Security("jwt")
-				})
-			})
-			apidsl.Resource("three", func() {
-				apidsl.Action("fifth", func() {
-					apidsl.Routing(apidsl.GET("/fifth"))
-				})
-			})
-			apidsl.Resource("auth", func() {
-				apidsl.NoSecurity()
-
-				apidsl.Action("auth", func() {
-					apidsl.Routing(apidsl.GET("/auth"))
-				})
-				apidsl.Action("refresh", func() {
-					apidsl.Routing(apidsl.GET("/refresh"))
-					apidsl.Security("jwt")
-				})
-			})
-
-			dslengine.Run()
-
-			Ω(dslengine.Errors).ShouldNot(HaveOccurred())
-			Ω(design.Design.SecuritySchemes).Should(HaveLen(2))
-			Ω(design.Design.Resources["one"].Actions["first"].Security).Should(BeNil())
-			Ω(design.Design.Resources["one"].Actions["second"].Security.Scheme.SchemeName).Should(Equal("jwt"))
-			Ω(design.Design.Resources["two"].Actions["third"].Security.Scheme.SchemeName).Should(Equal("password"))
-			Ω(design.Design.Resources["two"].Actions["fourth"].Security.Scheme.SchemeName).Should(Equal("jwt"))
-			Ω(design.Design.Resources["three"].Actions["fifth"].Security.Scheme.SchemeName).Should(Equal("jwt"))
-			Ω(design.Design.Resources["auth"].Actions["auth"].Security).Should(BeNil())
-			Ω(design.Design.Resources["auth"].Actions["refresh"].Security.Scheme.SchemeName).Should(Equal("jwt"))
 		})
+		apidsl.Resource("three", func() {
+			apidsl.Action("fifth", func() {
+				apidsl.Routing(apidsl.GET("/fifth"))
+			})
+		})
+		apidsl.Resource("auth", func() {
+			apidsl.NoSecurity()
+
+			apidsl.Action("auth", func() {
+				apidsl.Routing(apidsl.GET("/auth"))
+			})
+			apidsl.Action("refresh", func() {
+				apidsl.Routing(apidsl.GET("/refresh"))
+				apidsl.Security("jwt")
+			})
+		})
+		if err := dslengine.Run(); err != nil {
+			t.Fatal(err)
+		}
+
+		if len(design.Design.SecuritySchemes) != 2 {
+			t.Errorf("SecuritySchemes = %v; want 2", design.Design.SecuritySchemes)
+		}
+		if v := design.Design.Resources["one"].Actions["first"].Security; v != nil {
+			t.Errorf("Resources[one].Actions[first].Security = nil; want nil")
+		}
+		if v := design.Design.Resources["one"].Actions["second"].Security.Scheme.SchemeName; v != "jwt" {
+			t.Errorf("Resources[one].Actions[second].Security.Scheme.SchemeName = %v; want jwt", v)
+		}
+		if v := design.Design.Resources["two"].Actions["third"].Security.Scheme.SchemeName; v != "password" {
+			t.Errorf("Resources[two].Actions[third].Security.Scheme.SchemeName = %v; want password", v)
+		}
+		if v := design.Design.Resources["two"].Actions["fourth"].Security.Scheme.SchemeName; v != "jwt" {
+			t.Errorf("Resources[two].Actions[fourth].Security.Scheme.SchemeName = %v; want jwt", v)
+		}
+		if v := design.Design.Resources["three"].Actions["fifth"].Security.Scheme.SchemeName; v != "jwt" {
+			t.Errorf("Resources[three].Actions[fifth].Security.Scheme.SchemeName = %v; want jwt", v)
+		}
+		if v := design.Design.Resources["auth"].Actions["auth"].Security; v != nil {
+			t.Errorf("Resources[auth].Actions[auth].Security = nil; want nil")
+		}
+		if v := design.Design.Resources["auth"].Actions["refresh"].Security.Scheme.SchemeName; v != "jwt" {
+			t.Errorf("Resources[auth].Actions[refresh].Security.Scheme.SchemeName = %v; want jwt", v)
+		}
 	})
-})
+}
+
+func TestSecurity_basic(t *testing.T) {
+	t.Run("should fail because of duplicate in declaration", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.BasicAuthSecurity("broken_basic_authz", func() {
+				apidsl.Description("desc")
+				apidsl.Header("Authorization")
+				apidsl.Query("access_token")
+			})
+		})
+		if err := dslengine.Run(); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+
+	t.Run("should fail because of invalid declaration of OAuth2Flow", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.BasicAuthSecurity("broken_basic_authz", func() {
+				apidsl.Description("desc")
+				apidsl.ImplicitFlow("invalid")
+			})
+		})
+		if err := dslengine.Run(); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+
+	t.Run("should fail because of invalid declaration of TokenURL", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.BasicAuthSecurity("broken_basic_authz", func() {
+				apidsl.Description("desc")
+				apidsl.TokenURL("/token")
+			})
+		})
+		if err := dslengine.Run(); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+
+	t.Run("should fail because of invalid declaration of Header", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.BasicAuthSecurity("broken_basic_authz", func() {
+				apidsl.Description("desc")
+				apidsl.Header("invalid")
+			})
+		})
+		if err := dslengine.Run(); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+}
+
+func TestSecurity_OAuth2(t *testing.T) {
+	t.Run("should pass with valid values when well defined", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.Host("example.com")
+			apidsl.Scheme("http")
+			apidsl.OAuth2Security("googAuthz", func() {
+				apidsl.Description("Use Goog's Auth")
+				apidsl.AccessCodeFlow("/auth", "/token")
+				apidsl.Scope("scope:1", "Desc 1")
+				apidsl.Scope("scope:2", "Desc 2")
+			})
+		})
+		apidsl.Resource("one", func() {
+			apidsl.Action("first", func() {
+				apidsl.Routing(apidsl.GET("/first"))
+				apidsl.Security("googAuthz", func() {
+					apidsl.Scope("scope:1")
+				})
+			})
+		})
+		if err := dslengine.Run(); err != nil {
+			t.Fatal(err)
+		}
+
+		scheme := design.Design.SecuritySchemes[0]
+		if scheme.Description != "Use Goog's Auth" {
+			t.Errorf("Description = %v; want Use Goog's Auth", scheme.Description)
+		}
+		if scheme.AuthorizationURL != "http://example.com/auth" {
+			t.Errorf("AuthorizationURL = %v; want http://example.com/auth", scheme.AuthorizationURL)
+		}
+		if scheme.TokenURL != "http://example.com/token" {
+			t.Errorf("TokenURL = %v; want http://example.com/token", scheme.TokenURL)
+		}
+		if scheme.Flow != "accessCode" {
+			t.Errorf("Flow = %v; want accessCode", scheme.Flow)
+		}
+		if scheme.Scopes["scope:1"] != "Desc 1" {
+			t.Errorf("Scopes[scope:1] = %v; want Desc 1", scheme.Scopes["scope:1"])
+		}
+		if scheme.Scopes["scope:2"] != "Desc 2" {
+			t.Errorf("Scopes[scope:2] = %v; want Desc 2", scheme.Scopes["scope:2"])
+		}
+	})
+
+	t.Run("should fail because of invalid declaration of Header", func(t *testing.T) {
+		dslengine.Reset()
+		apidsl.API("", func() {
+			apidsl.OAuth2Security("googAuthz", func() {
+				apidsl.Header("invalid")
+			})
+		})
+		if err := dslengine.Run(); err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
+}
