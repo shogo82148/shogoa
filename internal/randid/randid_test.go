b55ef78af6446fdd064f4eba1ccf6e399a1aa25c
ICHINOSE Shogo <shogo82148@gmail.com>
2024-10-06 16:11:17 +0900

    Refactor randid_test.go to remove unnecessary log statement

--- internal/randid/randid_test.go
+++ internal/randid/randid_test.go
@@ -1,7 +1,6 @@
 package randid
 
 import (
-	"log"
 	"regexp"
 	"testing"
 )
@@ -10,7 +9,6 @@ func TestNew(t *testing.T) {
 	re := regexp.MustCompile(`^[a-zA-Z0-9]{24}$`)
 	for range 100 {
 		id := New(24)
-		log.Println(id)
 		if !re.MatchString(id) {
 			t.Errorf("invalid id: %s", id)
 		}
